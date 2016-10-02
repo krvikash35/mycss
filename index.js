bc3c36cd373e0cc32030a30373aa37d4619ba8ce
Vikash <krvikash35@gmail.com>
2016-10-02 21:13:34 +0530

    updated ripple effect

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+'use strict'
 let exp = require('express');
 let app = exp();
-app.listen('8080', function(){
-  console.log('mycss server listening on 8080');
+app.listen('8080', function() {
+    console.log('mycss server listening on 8080');
 })
 
-app.use( exp.static( __dirname ) );
+app.use(exp.static(__dirname));
